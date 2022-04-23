ad5f92b0a591650a556952e8109740db74e29199
Emily X <40601891+emilyxietty@users.noreply.github.com>
2022-04-23 19:50:18 -0400

    Add files via upload

---
+++ chineseish/content.js
@@ -0,0 +1,20 @@
+var elements = document.getElementsByTagName('*');
+
+function replaceWord(original, newword) {
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
+
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+
+                var replacedText = text.replace(original, newword);
+
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+            }
+        }
+    }
+}
